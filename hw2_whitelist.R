8694fcbc1e586773241b4833d98c82c105828d64
rundel <noreply@github.com>
2019-10-20 00:04:12 +0000

    Results of running styler:
    Styling  2  files:
     hw2_whitelist.R ℹ
     hw2.Rmd         ℹ
    ────────────────────────────────────────
    Status	Count	Legend
    ✔ 	0	File unchanged.
    ℹ 	2	File changed.
    ✖ 	0	Styling threw an error.
    ────────────────────────────────────────
    Please review the changes carefully!

--- hw2_whitelist.R
+++ hw2_whitelist.R
@@ -1,21 +1,24 @@
-allowed_files = c("hw2.md",
-                  "hw2.Rmd",
-                  "hw2.Rproj",
-                  "hw2_whitelist.R",
-                  "README.md",
-                  "data",
-                  "hw2_files",
-                  ".gitignore")
+allowed_files <- c(
+  "hw2.md",
+  "hw2.Rmd",
+  "hw2.Rproj",
+  "hw2_whitelist.R",
+  "README.md",
+  "data",
+  "hw2_files",
+  ".gitignore"
+)
 
-files = dir()
-disallowed_files = !files %in% allowed_files
+files <- dir()
+disallowed_files <- !files %in% allowed_files
 
 if (any(disallowed_files)) {
   cat("Disallowed files found:\n")
   cat("  (remove the following files from your repo)\n\n")
 
-  for(file in files[disallowed_files])
-    cat("*",file,"\n")
+  for (file in files[disallowed_files]) {
+    cat("*", file, "\n")
+  }
 
-  quit("no",1,FALSE)
+  quit("no", 1, FALSE)
 }
